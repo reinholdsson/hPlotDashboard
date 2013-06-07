d8b76ebdf2bf2ed7a111c0943d4cef971af5461c
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 15:47:59 +0200

    add gridster divs

--- ui.R
+++ ui.R
@@ -3,14 +3,17 @@ require(rCharts)
 require(shinyGridster)
 
 shinyUI(bootstrapPage(
-    div(class = 'container',
-        div(class = 'row',
-            div(class = 'span5',
-                showOutput('mychart', 'highcharts')
-            ),
-            div(class = 'span5',
-                showOutput('mychart2', 'highcharts')
-            )
+    gridster(width = 250, height = 250,
+        gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+            showOutput('mychart', 'highcharts')
         )
+    ),
+    gridster(width = 250, height = 250,
+        gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+            showOutput('mychart2', 'highcharts')
+        )
+    ),
+    gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+        h2("gridster test item")
     )
 ))
