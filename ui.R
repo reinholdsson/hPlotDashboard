53b89727bf96e79d3252c3ae1b6256d93b4a1dc9
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 17:05:57 +0200

    fixed wrong gridster syntax

--- ui.R
+++ ui.R
@@ -6,14 +6,12 @@ shinyUI(bootstrapPage(
     gridster(width = 250, height = 250,
         gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
             showOutput('mychart', 'highcharts')
-        )
-    ),
-    gridster(width = 250, height = 250,
+        ),
         gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
             showOutput('mychart2', 'highcharts')
+        ),
+        gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+            h2("gridster test item")
         )
-    ),
-    gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
-        h2("gridster test item")
     )
 ))
