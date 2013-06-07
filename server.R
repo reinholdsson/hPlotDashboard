b70b944f751365146ecdc57bd80ba39acd8ce7e8
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 16:02:35 +0200

    added some example plots

--- server.R
+++ server.R
@@ -2,26 +2,42 @@ require(shiny)
 require(rCharts)
 require(data.table)
 
-shinyServer(function(input, output, session){
+shinyServer(function(input, output){
 
-    output$mychart <- renderChart({
+    output$chart1 <- renderChart({
         a <- hPlot(x = 'Height', y = 'Pulse', data = MASS::survey, type = 'scatter', group = 'Sex', radius = 6)
         a$chart(backgroundColor = NULL, className = "mychart")
         a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
         a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
         a$plotOptions(scatter = list(marker = list(symbol = 'circle')))
-        a$set(dom = 'mychart')
+        a$set(dom = 'chart1')
         return(a)
     })
     
-    output$mychart2 <- renderChart({
+    output$chart2 <- renderChart({
         survey <- as.data.table(MASS::survey)
         freq <- survey[ , .N, by = c('Sex', 'Smoke')]
         a <- hPlot(x = 'Smoke', y = 'N', data = freq, type = 'column', group = 'Sex')
         a$chart(backgroundColor = NULL)
         a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
         a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
-        a$set(dom = 'mychart2')
+        a$set(dom = 'chart2')
+        return(a)
+    })
+    
+    output$chart3 <- renderChart({
+        a <- hPlot(Pulse ~ Height, data = MASS::survey, type = "bubble", title = "Zoom demo", subtitle = "bubble chart", size = "Age", group = "Exer")
+        a$chart(zoomType = "xy")
+        a$set(dom = 'chart3')
+        return(a)
+    })
+    
+    output$chart4 <- renderChart({
+        survey <- as.data.table(MASS::survey)
+        freq <- survey[ , .N, by = c('Smoke')]
+        a <- hPlot(x = "Smoke", y = "N", data = freq, type = "pie")
+        a$plotOptions(pie = list(size = 150))
+        a$set(dom = 'chart4')
         return(a)
     })
 })
--- ui.R
+++ ui.R
@@ -5,12 +5,12 @@ require(shinyGridster)
 shinyUI(bootstrapPage(
     div(class = 'container',
         div(class = 'row',
-            div(class = 'span5',
-                showOutput('mychart', 'highcharts')
-            ),
-            div(class = 'span5',
-                showOutput('mychart2', 'highcharts')
-            )
+            div(class = 'span5', showOutput('chart1', 'highcharts')),
+            div(class = 'span5', showOutput('chart2', 'highcharts'))
+        ),
+        div(class = 'row',
+            div(class = 'span5', showOutput('chart3', 'highcharts')),
+            div(class = 'span5', showOutput('chart4', 'highcharts'))
         )
     )
 ))
