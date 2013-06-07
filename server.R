6d4b11581d82fa49033429d20dcce7d33353466c
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 20:18:40 +0200

    add gridster

--- server.R
+++ server.R
@@ -6,10 +6,10 @@ shinyServer(function(input, output){
 
     output$chart1 <- renderChart({
         a <- hPlot(x = 'Height', y = 'Pulse', data = MASS::survey, type = 'scatter', group = 'Sex', radius = 6)
-        a$chart(backgroundColor = NULL, className = "mychart")
         a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
         a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
         a$plotOptions(scatter = list(marker = list(symbol = 'circle')))
+        a$chart(backgroundColor = NULL)
         a$set(dom = 'chart1')
         return(a)
     })
@@ -19,8 +19,6 @@ shinyServer(function(input, output){
         freq <- survey[ , .N, by = c('Sex', 'Smoke')]
         a <- hPlot(x = 'Smoke', y = 'N', data = freq, type = 'column', group = 'Sex')
         a$chart(backgroundColor = NULL)
-        a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
-        a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
         a$set(dom = 'chart2')
         return(a)
     })
@@ -28,6 +26,7 @@ shinyServer(function(input, output){
     output$chart3 <- renderChart({
         a <- hPlot(Pulse ~ Height, data = MASS::survey, type = "bubble", title = "Zoom demo", subtitle = "bubble chart", size = "Age", group = "Exer")
         a$chart(zoomType = "xy")
+        a$chart(backgroundColor = NULL)
         a$set(dom = 'chart3')
         return(a)
     })
@@ -37,6 +36,7 @@ shinyServer(function(input, output){
         freq <- survey[ , .N, by = c('Smoke')]
         a <- hPlot(x = "Smoke", y = "N", data = freq, type = "pie")
         a$plotOptions(pie = list(size = 150))
+        a$chart(backgroundColor = NULL)
         a$set(dom = 'chart4')
         return(a)
     })
--- ui.R
+++ ui.R
@@ -4,13 +4,23 @@ require(shinyGridster)
 
 shinyUI(bootstrapPage(
     div(class = 'container',
-        div(class = 'row',
-            div(class = 'span5', showOutput('chart1', 'highcharts')),
-            div(class = 'span5', showOutput('chart2', 'highcharts'))
-        ),
-        div(class = 'row',
-            div(class = 'span5', showOutput('chart3', 'highcharts')),
-            div(class = 'span5', showOutput('chart4', 'highcharts'))
+        gridster(width = 400, height = 400,
+            gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1, 
+                style="background: lightgoldenrodyellow;",
+                showOutput('chart1', 'highcharts')
+            ),
+            gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1, 
+                style="background: lightgoldenrodyellow;",
+                showOutput('chart2', 'highcharts')
+            ),
+            gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1, 
+                style="background: lightgoldenrodyellow;",
+                showOutput('chart3', 'highcharts')
+            ),
+            gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+                style="background: lightgoldenrodyellow;",
+                showOutput('chart4', 'highcharts')
+            )
         )
     )
 ))
